db1547587e83d44a8394b8c0b3ea980cafab5d59
jihun <gn753@naver.com>
2024-11-23 16:36:51 +0900

    chore: 프록시 서버 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://front-mission.bigs.or.kr", // 실제 API 서버 주소
+        changeOrigin: true, // 원본 도메인을 API 서버 도메인으로 변경
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });

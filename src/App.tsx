e570f8492dfe61447bc6bc0311eb8daee7943168
jihun <gn753@naver.com>
2024-11-25 21:39:56 +0900

    chore: update app

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,34 @@ import "./App.css";
 import SignupPage from "@/pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "@/pages/HomePage";
+import PostDetailPage from "@/pages/PostDetailPage";
+import PostEditPage from "@/pages/PostEditPage";
+import PostCreatePage from "./pages/PostCreatePage";
+import { AuthProvider } from "@/context/AuthProvider";
+import { CategoriesProvider } from "@/context/CategoriesContext";
+
+const AppContent = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/posts/:id" element={<PostDetailPage />} />
+      <Route path="/posts/edit/:id" element={<PostEditPage />} />
+      <Route path="/posts/create" element={<PostCreatePage />} />
+    </Routes>
+  );
+};
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
+        <AuthProvider>
+          <CategoriesProvider>
+            <AppContent />
+          </CategoriesProvider>
+        </AuthProvider>
       </BrowserRouter>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ChakraProvider >
-      <App />
-    </ChakraProvider>
-  </StrictMode>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );
